e0e497f22cb883d288bac88233d16f1563a7a0e7
Kyekyeku <safo.oppong@hotmail.com>
2024-05-12 15:43:44 +0000

    horizotal bar removed

--- play.js
+++ play.js
@@ -38,18 +38,3 @@ menuToggle.addEventListener('click', () => {
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     this.classList.toggle('active');
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    var colorPicker = document.getElementById('colorPicker');
-
-    colorPicker.addEventListener('change', function() {
-        var selectedColor = colorPicker.value;
-
-        // Set the background color of the header
-        var header = document.querySelector('header');
-        header.style.backgroundColor = selectedColor;
-
-        // Set the background color of the footer
-        
-    });
-});
