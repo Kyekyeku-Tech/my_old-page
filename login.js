13ebbd0ae1925ca67e45c5ce2357417ff20e9c5e
Kyekyeku <safo.oppong@hotmail.com>
2024-05-18 17:54:32 +0000

    new update for Agents

---
+++ login.js
@@ -0,0 +1,21 @@
+const validCredentials = [
+    { username: 'user1@example.com', password: 'password1' },
+    { username: 'user2@example.com', password: 'password2' },
+    { username: 'user3@example.com', password: 'password3' },
+    { username: 'safo.oppong@hotmail.com', password: 'Oppong1' }
+  ];
+
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    const isValid = validCredentials.some(cred => cred.username === username && cred.password === password);
+    
+    if (isValid) {
+      window.location.href = 'agent_mtn.html';
+    } else {
+      alert('Incorrect credentials, please wait for approval.');
+    }
+  });
